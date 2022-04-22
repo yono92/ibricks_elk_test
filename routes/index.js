275bb166c2d074ca811a04ba862efad170745797
yoonsejong <oxwk123@gmail.com>
2022-04-22 13:12:14 +0900

    first

---
+++ routes/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+var router = express.Router();
+var router = express.Router();
+const elasticsearch = require("../util/elasticClient");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/search_text", async (req, res) => {
+  console.log(req.body.keyword);
+  console.log(req.body.keyword2);
+  const a = await elasticsearch.search({
+    index: "practice",
+    body: {
+      query: {
+        bool: {
+          must: [
+            {
+              match_phrase_prefix: {
+                reporter: req.body.keyword,
+              },
+            },
+            {
+              match: {
+                update_dttm: req.body.keyword2,
+              },
+            },
+          ],
+        },
+      },
+    },
+  });
+  res.send(a);
+});
+
+router.post("/search_text_date", async (req, res) => {
+  console.log(req.body.text);
+  console.log(req.body.startdate);
+  console.log(req.body.findate);
+  const aa = await elasticsearch.search({
+    index: "practice",
+    body: {
+      query: {
+        bool: {
+          must: [
+            {
+              match_phrase_prefix: {
+                category: req.body.category,
+              },
+            },
+            {
+              range: {
+                start_dttm: {
+                  gte: req.body.startdate,
+                  lte: req.body.findate,
+                },
+              },
+            },
+          ],
+        },
+      },
+    },
+  });
+  res.send(aa);
+});
+
+module.exports = router;
---
+++ util/elasticClient.js
@@ -0,0 +1,6 @@
+const elasticsearch = require("elasticsearch");
+const client = new elasticsearch.Client({
+  hosts: ["http://localhost:9200"],
+});
+
+module.exports = client;
