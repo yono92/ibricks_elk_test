45bcaf69945b7863cbee3272a5639c84b9b06613
yoonsejong <oxwk123@gmail.com>
2022-04-28 14:39:12 +0900

    통합 검색으로 수정중

--- routes/index.js
+++ routes/index.js
@@ -7,202 +7,108 @@ const elasticsearch = require("../util/elasticClient");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "한국경제 검색" });
 });
 
 router.post("/search", async (req, res) => {
-  const q = req.body.word;
+  let q = req.body.word;
+  let sd = req.body.startdate;
+  let fd = req.body.findate;
   console.log(q);
-  let category = req.body.category;
-  console.log(category);
-  // 본문 내용으로 검색
-  if (category === "content") {
+  console.log(sd);
+  console.log(fd);
+  console.log("통합검색");
+  // 통합 검색 쿼리
+  if (q != null && sd === "null" && fd === "null") {
+    console.log("통합 검색");
     try {
       const a = await elasticsearch.search({
         index: "practice_ngram_nori",
         body: {
-          from: 0,
           size: 10,
+          from: 0,
           query: {
             multi_match: {
               query: q,
               fields: [
-                "content.nori",
-                "content.ngram",
                 "reporter.ngram",
-                "title.nori",
                 "title.ngram",
+                "title.nori",
+                "content.ngram",
+                "content.nori",
               ],
             },
           },
-          _source: ["content", "title", "reporter"],
+          _source: ["content", "reporter", "title", "start_dttm"],
         },
       });
-
-      console.log(jsonstr);
       let pageInfo = 0;
       res.render("result", {
         pageInfo: pageInfo,
         data: a.hits.hits,
         q: q,
-        category: category,
         totaldata: a,
       });
-      // res.send(a.hits.hits);
       console.log(a);
     } catch (error) {
-      res.status(500).json({
-        message: "ELS 서버 에러",
-      });
-    }
-  } else if (category === "giza") {
-    console.log(q);
-    // 기자 이름으로 검색
-    try {
-      const aa = await elasticsearch.search({
-        index: "practice_ngram_nori",
-        body: {
-          size: "100",
-          query: {
-            match_phrase_prefix: {
-              "reporter.ngram": q,
-            },
-          },
-          _source: ["reporter", "title", "content"],
-        },
-      });
-      // res.send(aa.hits.hits);
-      res.render("result", { data: aa.hits.hits });
-    } catch (error) {
-      res.status(500).json({
-        message: "ELS 서버 에러",
-      });
-    }
-  } else if (category === "date") {
-    console.log(req.body.startdate);
-    console.log(req.body.findate);
-    console.log(q);
-    // 날짜 범위별 검색
-    try {
-      const aaa = await elasticsearch.search({
-        index: "practice_ngram_nori",
-        body: {
-          query: {
-            bool: {
-              must: [
-                {
-                  match: {
-                    "content.nori": q,
-                  },
-                },
-                {
-                  range: {
-                    start_dttm: {
-                      gte: req.body.startdate,
-                      lte: req.body.findate,
-                    },
-                  },
-                },
-              ],
-            },
-          },
-          _source: ["reporter", "content", "start_dttm", "title"],
-        },
-      });
-      // res.send(aaa.hits.hits);
-      res.render("result", { data: aaa.hits.hits });
-    } catch (error) {
-      res.status(500).json({
-        message: "ELS 서버 에러",
-      });
-    }
-  } else if (category === "tot") {
-    // 기간 내용 기자 검색
-    console.log(req.body.startdate);
-    console.log(req.body.findate);
-    try {
-      const aaaa = await elasticsearch.search({
-        index: "practice_ngram",
-        body: {
-          query: {
-            bool: {
-              must: [
-                {
-                  range: {
-                    update_dttm: {
-                      gte: req.body.startdate,
-                      lte: req.body.findate,
-                    },
-                  },
-                },
-                {
-                  match_phrase_prefix: {
-                    "reporter.ngram": req.body.reporter,
-                  },
-                },
-                {
-                  match_phrase_prefix: {
-                    content: q,
-                  },
-                },
-              ],
-            },
-          },
-          _source: ["reporter", "content", "start_dttm", "title"],
-        },
-      });
-      res.render("result", { data: aaaa.hits.hits });
-      // res.send(aaaa)
-    } catch (error) {
-      res.status(500).json({
-        message: "ELS 서버 에러",
-      });
-    }
-  } else if (category === "hyung") {
-    console.log("형태소분석 시작");
-    // 간단한 형태소 분석 연습
-    try {
-      const aaaaa = await elasticsearch.search({
-        body: {
-          query: {
-            totokenizer: ngram,
-            text: q,
-            explain: "true",
-          },
-        },
-      });
-      console.log(aaaaa);
-      res.render("result", { data: aaaaa.hits.hits });
-      // res.send(aaaaa);
-      console.log("끝");
-    } catch (error) {
-      res.status(500).json({
-        message: "ELS 서버 에러",
-      });
-    }
-  } else if (category === "tite") {
-    console.log("제목별 검색");
-    console.log(q);
-    try {
-      const aaaaaa = await elasticsearch.search({
-        index: "practice_ngram_nori",
-        body: {
-          query: {
-            match: {
-              "title.nori": q,
-            },
-          },
-          _source: ["reporter", "content", "title"],
-        },
-      });
-      // res.send(aaaaaa);
-      res.render("result", { data: aaaaaa.hits.hits });
-    } catch (error) {
-      res.status(500).json({
-        message: "ELS 서버 에러",
-      });
+      res.render("error");
     }
   }
+
+  // if (q !== null && sd !== null && fd !== null) {
+  //   console.log("날짜조건 검색");
+  //   try {
+  //     const aa = await elasticsearch.search({
+  //       index: "practice_ngram_nori",
+  //       body: {
+  //         size: 10,
+  //         from: 0,
+  //         query: {
+  //           bool: {
+  //             must: [
+  //               {
+  //                 multi_match: {
+  //                   query: q,
+  //                   fields: [
+  //                     "content.ngram",
+  //                     "content.nori",
+  //                     "title.ngram",
+  //                     "title.nori",
+  //                     "reporter.ngram",
+  //                   ],
+  //                 },
+  //               },
+  //               {
+  //                 range: {
+  //                   start_dttm: {
+  //                     gte: sd,
+  //                     lte: fd,
+  //                   },
+  //                 },
+  //               },
+  //             ],
+  //           },
+  //         },
+  //       },
+  //       _source: ["content", "title", "start_dttm", "reporter"],
+  //     });
+  //     let pageInfo = 0;
+  //     res.render("result", {
+  //       pageInfo: pageInfo,
+  //       data: aa.hits.hits,
+  //       q: q,
+  //       totaldata: a,
+  //       sd: sd,
+  //       fd: fd,
+  //     });
+  //   } catch (error) {
+  //     console.log("캐치에서 빠짐");
+  //     res.render("error");
+  //   }
+  // } else {
+  //   console.log("엘스에서 빠짐");
+  //   res.render("error");
+  // }
 });
 
 router.get("/search", async (req, res) => {
@@ -235,6 +141,8 @@ router.get("/search", async (req, res) => {
       category: paramdata.category,
       q: paramdata.q,
     });
+  } else {
+    res.render("error");
   }
 });
 
