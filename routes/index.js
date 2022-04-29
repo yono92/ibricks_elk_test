2afae825384e35b8914d12fd4bb3882a52a8ed22
yoonsejong <oxwk123@gmail.com>
2022-04-29 10:23:07 +0900

    기간별 검색 및 SORT작업

--- routes/index.js
+++ routes/index.js
@@ -13,80 +13,198 @@ router.get("/", function (req, res, next) {
 
 router.post("/search", async (req, res) => {
   // 바디에서 받은 검색어
-  const q = req.body.word;
-  //바디에서 받은 시작일
-  // const sd = req.body.startdate;
-  //바디에서 받은 종료일
-  // const fd = req.body.findate;
-  // 검색어 로그
+  let q = req.body.word;
   console.log(q);
-  // console.log(sd);
-  // console.log(fd);
+
   // 통합 검색 시작
   console.log("통합 검색");
-  try {
-    const a = await elasticsearch.search({
-      index: "practice_ngram_nori",
-      body: {
-        size: 10,
-        from: 0,
-        query: {
-          multi_match: {
-            query: q,
-            fields: [
-              "content.ngram",
-              "content.nori",
-              "title.ngram",
-              "title.nori",
-              "reporter.ngram",
-            ],
-          },
+  let defaultsort = "desc";
+  const a = await elasticsearch.search({
+    index: "practice_ngram_nori",
+    body: {
+      size: 10,
+      from: 0,
+      query: {
+        multi_match: {
+          query: q,
+          fields: [
+            "content.ngram",
+            "content.nori",
+            "title.ngram",
+            "title.nori",
+            "reporter.ngram",
+          ],
         },
-        _source: ["reporter", "content", "title", "start_dttm"],
       },
-    });
-    console.log(a);
-    console.log(a.hits.hits);
-    let pageInfo = 0;
-    res.render("result", {
-      pageInfo: pageInfo,
-      data: a.hits.hits,
-      q: q,
-      totaldata: a,
-    });
-  } catch (error) {
-    res.render("error");
-  }
+      sort: [{ start_dttm: defaultsort }, "_score"],
+      _source: ["reporter", "content", "title", "start_dttm"],
+    },
+  });
+  console.log(a);
+  console.log(a.hits.hits);
+  let pageInfo = 1;
+  let startdate = "";
+  let findate = "";
+  res.render("result", {
+    pageInfo: pageInfo,
+    data: a.hits.hits,
+    q: q,
+    totaldata: a,
+    sort_data: defaultsort,
+    startdate: startdate,
+    findate: findate,
+  });
 });
 
 router.get("/search", async (req, res) => {
   let paramdata = req.query;
-  const aa = await elasticsearch.search({
-    index: "practice_ngram_nori",
-    body: {
-      from: paramdata.pageInfo,
-      size: 10,
-      query: {
-        multi_match: {
-          query: paramdata.q,
-          fields: [
-            "content.nori",
-            "content.ngram",
-            "reporter.ngram",
-            "title.nori",
-            "title.ngram",
-          ],
+  // console.log(paramdata.startdate);
+  // console.log(paramdata.findate);
+  let defaultsort = "";
+  console.log(defaultsort.sort_data);
+  if (paramdata.sort_data == null) {
+    defaultsort = "desc";
+  } else {
+    defaultsort = paramdata.sort_data;
+  }
+  if ((paramdata.startdate = "" || paramdata.findate == "")) {
+    const a = await elasticsearch.search({
+      index: "practice_ngram_nori",
+      body: {
+        from: (paramdata.pageInfo - 1) * 10,
+        size: 10,
+        query: {
+          bool: {
+            must: [
+              {
+                multi_match: {
+                  query: paramdata.q,
+                  fields: [
+                    "reporter.ngram",
+                    "title.nori",
+                    "content.nori",
+                    "content.ngram",
+                    "title.ngram",
+                  ],
+                },
+              },
+            ],
+          },
         },
+        sort: [{ start_dttm: defaultsort }, "_score"],
+        _source: ["reporter", "start_dttm", "title", "content"],
       },
-      _source: ["content", "title", "reporter", "start_dttm"],
-    },
-  });
-  res.render("result", {
-    pageInfo: paramdata.pageInfo,
-    data: aa.hits.hits,
-    totaldata: aa,
-    q: paramdata.q,
-  });
+    });
+    res.render("result", {
+      pageInfo: paramdata.pageInfo,
+      data: a.hits.hits,
+      totaldata: a,
+      q: paramdata.q,
+      sort_data: defaultsort,
+      startdate: paramdata.startdate,
+      findate: paramdata.findate,
+    });
+  } else {
+    const a = await elasticsearch.search({
+      index: "practice_ngram_nori",
+      body: {
+        from: (paramdata.pageInfo - 1) * 10,
+        size: 10,
+        query: {
+          bool: {
+            must: [
+              {
+                multi_match: {
+                  query: paramdata.q,
+                  fields: [
+                    "reporter.ngram",
+                    "title.nori",
+                    "content.nori",
+                    "content.ngram",
+                    "title.ngram",
+                  ],
+                },
+              },
+              {
+                range: {
+                  start_dttm: {
+                    gte: paramdata.startdate,
+                    lte: paramdata.findate,
+                  },
+                },
+              },
+            ],
+          },
+        },
+        sort: [{ start_dttm: defaultsort }, "_score"],
+        _source: ["reporter", "start_dttm", "title", "content"],
+      },
+    });
+    res.render("result", {
+      pageInfo: paramdata.pageInfo,
+      data: a.hits.hits,
+      totaldata: a,
+      q: paramdata.q,
+      sort_data: defaultsort,
+      startdate: paramdata.startdate,
+      findate: paramdata.findate,
+    });
+  }
+
+  // let paramdata = req.query;
+  // console.log(paramdata.startdate);
+  // console.log(paramdata.findate);
+  // let defaultsort = "";
+  // console.log(defaultsort.sort_data);
+  // if (paramdata.sort_data == null) {
+  //   defaultsort = "desc";
+  // } else {
+  //   defaultsort = paramdata.sort_data;
+  // }
+  // const a = await elasticsearch.search({
+  //   index: "practice_ngram_nori",
+  //   body: {
+  //     from: (paramdata.pageInfo - 1) * 10,
+  //     size: 10,
+  //     query: {
+  //       bool: {
+  //         must: [
+  //           {
+  //             multi_match: {
+  //               query: paramdata.q,
+  //               fields: [
+  //                 "reporter.ngram",
+  //                 "title.nori",
+  //                 "content.nori",
+  //                 "content.ngram",
+  //                 "title.ngram",
+  //               ],
+  //             },
+  //           },
+  //           {
+  //             range: {
+  //               start_dttm: {
+  //                 gte: paramdata.startdate,
+  //                 lte: paramdata.findate,
+  //               },
+  //             },
+  //           },
+  //         ],
+  //       },
+  //     },
+  //     sort: [{ start_dttm: defaultsort }, "_score"],
+  //     _source: ["reporter", "start_dttm", "title", "content"],
+  //   },
+  // });
+  // res.render("result", {
+  //   pageInfo: paramdata.pageInfo,
+  //   data: a.hits.hits,
+  //   totaldata: a,
+  //   q: paramdata.q,
+  //   sort_data: defaultsort,
+  //   startdate: paramdata.startdate,
+  //   findate: paramdata.findate,
+  // });
 });
 
 module.exports = router;
